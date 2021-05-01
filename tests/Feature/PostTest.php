c7616056d62d91971ee778e1a67244d37fe68960
LucasR-07 <lucasromero2705@gmail.com>
2021-05-01 18:01:57 -0300

    Post: add test Show + refactors

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,17 +2,21 @@
 
 namespace Tests\Feature;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class PostTest extends TestCase
 {
-    const baseURL = '/posts';
+    use RefreshDatabase;
+
+    const baseURL = 'posts';
 
     public function testIndex()
     {
-        $response = $this->get('posts');
+        $response = $this->json('GET', self::baseURL);
 
         $response->assertStatus(200);
         $response->assertJsonStructure(
@@ -27,5 +31,23 @@ class PostTest extends TestCase
                 ]
             ]
         );
+    }
+
+    public function testShow()
+    {
+        $user = User::factory()->create();
+        $post = Post::factory()
+            ->create(['user_id' => $user->id]);
+
+        $response = $this->json(
+            'GET',
+            self::baseURL . '/' . $post->id
+        );
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'title',
+            'content',
+            'slug',
+        ]);
     }
 }
